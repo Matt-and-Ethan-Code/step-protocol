d7e61706d099b79cc4c9621e8c07c8a2d1da369c
gaidukev <90803020+gaidukev@users.noreply.github.com>
2025-10-12 15:12:59 -0600

    registered model in admin.py

--- initial_screening/admin.py
+++ initial_screening/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import Questionnaire, QuestionBlock, Question, AnswerOption
 
-# Register your models here.
+class AnswerOptionInline(admin.TabularInline):
+    model = AnswerOption
+    extra = 1
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class QuestionBlockInline(admin.TabularInline):
+    model = QuestionBlock
+    extra = 1
+
+@admin.register(Questionnaire)
+class QuestionnaireAdmin(admin.ModelAdmin):
+    list_display = ('name', 'citation')
+    inlines = [QuestionBlockInline]
+
+@admin.register(QuestionBlock)
+class QuestionBlockAdmin(admin.ModelAdmin):
+    list_display = ('title', 'questionnaire', 'order', 'description')
+    inlines = [QuestionInline]
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question_block', 'question_type', 'order')
+    inlines = [AnswerOptionInline]
