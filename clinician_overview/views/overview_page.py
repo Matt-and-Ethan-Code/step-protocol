aebe89d2d8319982310335c12c1289e4bd0fc837
enortho <ethan@opgenorth.net>
2026-01-27 22:49:57 -0700

    add not clinician page

--- clinician_overview/views/overview_page.py
+++ clinician_overview/views/overview_page.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from django.contrib.auth.decorators import login_required
+from initial_screening.models import QuestionnaireResponse
+
 
 # Create your views here.
+@login_required
 def overview_page(request: HttpRequest) -> HttpResponse:
+  user = request.user
+  if not user_is_clinician(user):
+    return not_clinician()
   context = {}
+
   return render(request, "clinician_overview/overview_page.html", context=context)
 
 
+def not_clinician(request: HttpRequest) -> HttpResponse:
+  return render(request, "clinician_overview/not_clinician_page.html")
+
+def user_is_clinician(user_email: str):
+  clincian_emails = [
+    
+  ]
+  
+  return user_email in clincian_emails
