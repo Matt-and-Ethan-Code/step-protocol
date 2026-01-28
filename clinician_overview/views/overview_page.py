19df2ef2f710a02fe264a9f771b818e3d8ebd8c6
enortho <ethan@opgenorth.net>
2026-01-27 22:23:14 -0700

    add overview page, static resources

--- clinician_overview/views/overview_page.py
+++ clinician_overview/views/overview_page.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
-import django.http as http
+
 # Create your views here.
 def overview_page(request: HttpRequest) -> HttpResponse:
-  return HttpResponse("hiiiiaaa")
+  context = {}
+  return render(request, "clinician_overview/overview_page.html", context=context)
+
+
