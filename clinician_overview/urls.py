a257e9e8fc87417a0fc4dc5df5d7f6f00bf0b23f
enortho <ethan@opgenorth.net>
2026-03-13 22:10:21 -0600

    clients & client summary

--- clinician_overview/urls.py
+++ clinician_overview/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.http import HttpRequest, HttpResponse
-from clinician_overview.views import client_responses_page, questionnaire_response_page, not_clinician_page, notifications_page, clients_page
+from clinician_overview.views import client_responses_page, questionnaire_response_page, not_clinician_page, notifications_page, clients_page, client_summary_page
 from django.shortcuts import render, redirect
 
 def handle_index(_req: HttpRequest) -> HttpResponse:
@@ -10,6 +10,7 @@ urlpatterns = [
   path('', handle_index),
   path('notifications', notifications_page, name='notifications'),
   path('clients', clients_page, name='clients'),
+  path('clients/<str:client_id>', client_summary_page, name='client_summary'),
   path('response/<str:questionnaire_response_id>', questionnaire_response_page),
   path('clinician-required', not_clinician_page),
   path('esme', lambda r: render(r, 'clinician_overview/clinician_base_layout.html')),
--- clinician_overview/views/__init__.py
+++ clinician_overview/views/__init__.py
@@ -4,3 +4,4 @@ from .clinician_overview_page import clinician_overview_page # type: ignore
 from .not_clinician_page import not_clinician_page # type: ignore
 from .notifications_page import notifications_page # type: ignore
 from .clients_page import clients_page # type: ignore
+from .client_summary_page import client_summary_page # type: ignore
