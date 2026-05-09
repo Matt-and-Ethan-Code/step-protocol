f271d9c567fa9401b8d270e178d32e5b26b05fba
enortho <ethan@opgenorth.net>
2026-05-09 15:14:54 -0600

    comments for initial screening models

--- initial_screening/models.py
+++ initial_screening/models.py
@@ -2,11 +2,23 @@ from django.db import models
 from django.db.models import ForeignKey
 from clinician_overview.models import ClientId
 
-class Form(models.Model): 
+class Form(models.Model):
+    """
+    A whole area that a questionnaire can appear, ie Intake vs Post Exam vs Feedback form reference.
+    So this is like the whole "initial screening" questionnaire, which contains the DES-T, PCL-5, etc.
+    Like one big bundle of questionnaires.
+    Contains only the questionnaire data, not the responses associated (only the shape of the data.)
+    """
     id: int
     name = models.CharField(max_length=300, unique=True)
 
 class Questionnaire(models.Model):
+    """
+    A set of questions, like the DES-T. A questionnaire always appears on one page.
+    In the initial screening, each time you click 'Next' to go to the next page it is another
+    questionnaire object.
+    Contains references to the questions (QuestionBlock) but not the actual responses (that's QuestionnaireReponse).
+    """
     id: int
     name = models.CharField(max_length=300, unique=True)
     citation = models.TextField(blank=True)
@@ -31,7 +43,13 @@ class QuestionnaireResponse(models.Model):
     submitted_at = models.DateTimeField(auto_now_add=True)
     view_count = models.IntegerField(default=0, null=False)
     """The number of times a clinician has viewed the response."""
+
 class QuestionBlock(models.Model):
+    """
+    In a questionnaire, sometimes there is a little section like "for the next 3 questions, think about how often
+    it has occurred in the past month". A QuestionBlock is a set of questions that is logically grouped like that and
+    it is stored in the Questionnaire and "normal" questions are just in one big QuestionBlock.
+    """
     questionnaire = models.ForeignKey(
         Questionnaire,
         related_name='question_blocks',
@@ -90,6 +108,12 @@ class Question(models.Model):
         return displayText
 
 class AnswerOption(models.Model):
+    """
+    For multiple choice questions. Each question has their own set of AnswerOptions (ie, each is like "Extremely Often", "Somewhat Often").
+    internal_value is for the special case where you select a provider by name but you want the actual value to be their email (the user sees one thing
+    and inside its something else).
+    Text questions do not have an AnswerOption
+    """
     id: int
     question = models.ForeignKey(Question, related_name='options', on_delete=models.CASCADE)
     text = models.TextField()
@@ -103,6 +127,13 @@ class AnswerOption(models.Model):
         return self.text
 
 class ResponseItem(models.Model):
+    """
+    The response a user provides for an individual question. (For the full questionnaire, look at QuestionnaireResponse).
+    The answer field will probably always be present, mostly for convenience. If you are a text field, it contains the text
+    that the user entered. If it is a multiple choice question, it'll be the text of the option they clicked on (ie, 'Somewhat Likely'). Otherwise
+    it is blank.
+    The answerID is more what the code should be using (especially for multiple choice). It is the actual option that the user picked in the DB.
+    """
     response: ForeignKey[QuestionnaireResponse] = models.ForeignKey(QuestionnaireResponse, related_name='items', on_delete=models.CASCADE)
     question: ForeignKey[Question] = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer = models.TextField()
