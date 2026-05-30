3abf8156b293ce86be89942edc179ba7852f8b9f
enortho <ethan@opgenorth.net>
2026-05-29 21:34:02 -0600

    fix dest scoring (was doing des-II scoring instead)

--- initial_screening/scoring/dest.py
+++ initial_screening/scoring/dest.py
@@ -4,63 +4,25 @@ Use this: https://onlinelibrary.wiley.com/doi/pdf/10.1002/9781118093146.app1
 
 from dataclasses import dataclass
 from typing import Literal
-from .dest_types import DesTQuestion, DesTForm
+from .dest_types import DesTForm
 
 type DesTDescriptor = Literal['low', 'mild', 'moderate', 'high', 'very high']
 
 @dataclass
 class DesTScore:
-  amnesia_score: int
-  amnesia_descriptor: DesTDescriptor
-  depersonalisation_score: int
-  depersonalisation_descriptor: DesTDescriptor
-  absorption_score: int
-  absorption_descriptor: DesTDescriptor
-  total_score: int
+  score: int
+  significant: bool
 
-def amnesia_questions() -> list[DesTQuestion]:
-  return [3,4,5,8,25,26]
-def depersonalisation_questions() -> list[DesTQuestion]:
-  return [7,11,12,13,27,28]
-def absorption_questions() -> list[DesTQuestion]:
-  return [2,14,15,17,18,20]
+def score(responses: DesTForm) -> DesTScore: 
+  """
+  Returns the score, which is the mean of the responses.
+  Significant if the mean is >= 20%.
+  """
+  question_count = 8
+  score = sum(responses.keys()) // question_count # the average among the 8 questions
 
-def descriptor_from_score(score: int) -> DesTDescriptor:
-  if score <= 11:
-    return 'low'
-  elif score <= 19:
-    return 'mild'
-  elif score <= 29:
-    return 'moderate'
-  elif score <= 45:
-    return 'high'
-  else:
-    return 'very high'
-
-def score(responses: DesTForm) -> DesTScore:
-  def sum_map(questions: list[DesTQuestion]) -> int:
-    total = 0
-    for question in questions:
-      total += responses[question]
-    return total
-  
-  amnesia_score = sum_map(amnesia_questions())
-  amnesia_descriptor = descriptor_from_score(amnesia_score)
-
-  depersonalisation_score = sum_map(depersonalisation_questions())
-  depersonalisation_descriptor = descriptor_from_score(depersonalisation_score)
-
-  absorption_score = sum_map(absorption_questions())
-  absorption_descriptor = descriptor_from_score(absorption_score)
-  
-  total_score = amnesia_score + depersonalisation_score + absorption_score
   return DesTScore(
-    amnesia_score=amnesia_score,
-    amnesia_descriptor=amnesia_descriptor,
-    depersonalisation_score=depersonalisation_score,
-    depersonalisation_descriptor=depersonalisation_descriptor,
-    absorption_score=absorption_score,
-    absorption_descriptor=absorption_descriptor,
-    total_score=total_score
+    score=score,
+    significant=score >= 20
   )
 
