630ed1b4f7b2f6790804ceb26febd3ba7b879357
gaidukev <90803020+gaidukev@users.noreply.github.com>
2026-08-15 13:13:04 -0600

    added step together templates

--- step_together/urls.py
+++ step_together/urls.py
@@ -4,4 +4,5 @@ import step_together.views as views
 urlpatterns: list[URLPattern] = [
     path('agreement/', views.agreement_view, name='agreement'), 
     path('step-together/', views.step_together_portal_view, name='step-together'),
+    path('step-together/manual/', views.step_together_manual, name='step-together-manual')
 ]
