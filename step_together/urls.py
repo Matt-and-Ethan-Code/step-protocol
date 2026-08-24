916d5f490b40eb7c07d1ca553835d8bc025920e5
gaidukev <90803020+gaidukev@users.noreply.github.com>
2026-08-23 19:15:43 -0600

    added welcome-to-step-together, step-self-care-introduction and bilateral-tapping

--- step_together/urls.py
+++ step_together/urls.py
@@ -6,4 +6,7 @@ urlpatterns: list[URLPattern] = [
     path('step-together/', views.step_together_portal_view, name='step-together'),
     path('step-together/manual/', views.step_together_manual, name='step-together-manual'), 
     path('step-together/pregroup-checklist/', views.step_together_pregroup_checklist, name='step-together-pregroup-checklist'),
+    path('step-together/welcome-to-step-together/', views.welcome_to_step_together, name='welcome-to-step-together'),
+    path('step-together/self-care-introduction/', views.self_care_introduction, name='self-care-introduction'), 
+    path('step-together/bilateral-tapping/', views.bilateral_tapping, name='bilateral-tapping')
 ]
--- step_together/views.py
+++ step_together/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from step_solo.util.get_video_url import get_video_url
 
 from .forms import AgreementForm
 from .models import Agreement, ProviderConfirmation
@@ -18,6 +19,23 @@ def step_together_manual(request: HttpRequest) -> HttpResponse:
 def step_together_pregroup_checklist(request: HttpRequest) -> HttpResponse:
     return render(request, "step_together/step-together-pre-group-checklist.html", {
         "nav_section": "step-together"
+    })
+
+def welcome_to_step_together(request: HttpRequest) -> HttpResponse:
+    return render(request, "step_together/welcome-to-step-together.html", {
+        "nav_section": "step-together"
+    })
+
+def self_care_introduction(request:HttpRequest) -> HttpResponse:
+    return render(request, "step_together/self-care-introduction.html", {
+        "nav_section": "step-together", 
+        "video_url": get_video_url('st_self_care_introduction')
+    })
+
+def bilateral_tapping(request:HttpRequest) -> HttpResponse: 
+    return render(request, 'step_together/bilateral-tapping.html', {
+        'nav_section': 'step-together', 
+        'video_url': get_video_url('st_bilateral_tapping')
     })
 
 
